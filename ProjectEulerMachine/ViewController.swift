937057dd62b416dd6b7c07c50a03f6e859b658a1
vjosullivan <vjosullivan@gmail.com>
2016-01-23 07:30:24 +0000

    Initial Commit

---
+++ ProjectEulerMachine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectEulerMachine
+//
+//  Created by Vincent O'Sullivan on 23/01/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ProjectEulerMachineTests/ProjectEulerMachineTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProjectEulerMachineTests.swift
+//  ProjectEulerMachineTests
+//
+//  Created by Vincent O'Sullivan on 23/01/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectEulerMachine
+
+class ProjectEulerMachineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

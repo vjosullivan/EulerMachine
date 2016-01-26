86fc6227fec5059aa407194c3cd5a75ff4cb2ae2
vjosullivan <vjosullivan@gmail.com>
2016-01-26 21:32:24 +0000

    BigInteger improved.

--- ProjectEulerMachine/Common Functions/BigInteger.swift
+++ ProjectEulerMachine/Common Functions/BigInteger.swift
@@ -8,8 +8,8 @@
 
 struct BigInteger {
 
-    private var value = [0]
-    private var positive: Bool
+    let value: [Int]
+    let positive: Bool
 
     init(int: Int) {
         self.positive = (int >= 0)
@@ -24,6 +24,33 @@ struct BigInteger {
             }
             value = temp
         }
+    }
+
+    private init(array: [Int], positive: Bool) {
+        self.positive = positive
+        self.value = array
+    }
+
+    func add(other: BigInteger) -> BigInteger {
+        var index = 0
+        if self.positive == other.positive {
+            var result = [Int]()
+            var sum = 0
+            while index < self.value.count && index < other.value.count {
+                sum = self.value[index] + other.value[index]
+                result.append(sum % 10)
+                sum /= 10
+                index += 1
+            }
+            if sum > 0 { result.append(sum) }
+            return BigInteger(array: result, positive: positive)
+        } else {
+            return subtract(other)
+        }
+    }
+
+    func subtract(other: BigInteger) -> BigInteger {
+        return other
     }
 
     var description: String {
---
+++ ProjectEulerMachineTests/Common Functions/BigIntegerTests.swift
@@ -0,0 +1,42 @@
+//
+//  BigIntegerTests.swift
+//  ProjectEulerMachine
+//
+//  Created by Vincent O'Sullivan on 26/01/2016.
+//  Copyright © 2016 Vincent O'Sullivan. All rights reserved.
+//
+
+import XCTest
+
+class BigIntegerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testBICreatable() {
+        let z = BigInteger(int: 0)
+        XCTAssertNotNil(z)
+        XCTAssertEqual("0", z.description)
+    }
+
+    func testBIAdd01() {
+        let a = BigInteger(int: 12)
+        let b = BigInteger(int: 34)
+        let c = a.add(b)
+        XCTAssertEqual("46", c.description)
+    }
+
+    func testBIAdd02() {
+        let a = BigInteger(int: 123)
+        let b = BigInteger(int: 45)
+        let c = a.add(b)
+        XCTAssertEqual("168", c.description)
+    }
+}

efd10902ac2bd4613a603bef79405a4d95411a2a
vjosullivan <vjosullivan@gmail.com>
2016-01-29 09:23:44 +0000

    BigInteger addition refactored.

--- ProjectEulerMachine/Common Functions/BigInteger.swift
+++ ProjectEulerMachine/Common Functions/BigInteger.swift
@@ -19,6 +19,10 @@ struct BigInteger {
 
     // MARK: Functions
 
+    init() {
+        self.init(int: 0)
+    }
+
     init(int: Int) {
         self.positive = (int >= 0)
         if int == 0 {
@@ -32,12 +36,19 @@ struct BigInteger {
             }
             value = temp
         }
+        print("value = \(value)")
     }
 
     private init(array: [Int], positive: Bool) {
         self.positive = positive
         self.value = array
     }
+}
+
+// MARK: - Extensions
+// MARK: - CustomStringConverible extension
+
+extension BigInteger: CustomStringConvertible {
 
     var description: String {
         var desc = ""
@@ -48,42 +59,49 @@ struct BigInteger {
     }
 }
 
-// MARK: - Extensions
 // MARK: - Addition extension
 
 extension BigInteger {
 
     func add(other: BigInteger) -> BigInteger {
         if self.positive == other.positive {
-            if self.length >= other.length {
-                return BigInteger.add(self, to: other)
-            } else {
-                return BigInteger.add(other, to: self)
-            }
+            return BigInteger(array: BigInteger.add(self.value, to: other.value), positive: positive)
         } else {
             return subtract(other)
         }
     }
 
-    private static func add(a: BigInteger, to b: BigInteger) -> BigInteger {
-        // Pad b with leading zeros to make it same length as a.
-        let diff = a.length - b.length
-        var bValue = b.value
-        for _ in 0..<diff { bValue.append(0) }
+    private static func add(var a: [Int], var to b: [Int]) -> [Int] {
+
+        // Make a and b equal length.
+        padOut(a: &a, b: &b)
 
         var result = [Int]()
         var index = 0
         var carry = 0
-        while index < a.length {
-            carry += a.value[index] + bValue[index]
+        while index < a.count {
+            carry += a[index] + b[index]
             result.append(carry % 10)
             carry /= 10
             index += 1
         }
         if carry > 0 { result.append(carry) }
-        return BigInteger(array: result, positive: a.positive)
+        return result
+    }
+
+    ///  Pad the shorter of the two arguments (with zeroes) to an equal length to the longer.
+    ///
+    private static func padOut(inout a a: [Int], inout b: [Int]) {
+        let diff = a.count - b.count
+        if diff > 0 {
+            for _ in 0..<diff { b.append(0) }
+        } else {
+            for _ in 0..<(-diff) { a.append(0) }
+        }
     }
 }
+
+
 
 // MARK: Subtraction extension
 
@@ -98,8 +116,23 @@ extension BigInteger {
 
 extension BigInteger {
 
-    func multiply(byOther: BigInteger) -> BigInteger {
-        return BigInteger(int: 0)
+    func multiply(by other: BigInteger) -> BigInteger {
+        if self.description == "0" || other.description == "0" {
+            return BigInteger(int: 0)
+        } else {
+            var partResult = [Int]()
+            var result = BigInteger()
+            var product = 0
+            for sIndex in 0..<self.length {
+                for oIndex in 0..<other.length {
+                    product += self.value[sIndex] * other.value[oIndex]
+                    partResult[sIndex] = product % 10
+                    product /= 10
+                }
+                //result = result.add(<#T##other: BigInteger##BigInteger#>)
+            }
+            return BigInteger(int: 0)
+        }
     }
 }
 
